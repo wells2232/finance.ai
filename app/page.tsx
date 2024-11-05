266b684857583c5e3703b2d5113e124efe15b17e
Wellington Lopes Nunes <wellington.nunes10@hotmail.com>
2024-11-05 14:44:52 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button }  from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="p-5 text-amber-300">Home Page</h1>
+      <Button>Hello World</Button>
     </div>
   );
 }
