c0b4a99c28ba5144225b41109c1e96e152cb7e4f
Wellington Lopes Nunes <wellington.nunes10@hotmail.com>
2024-11-08 15:18:45 -0300

    feat: ensure transaction and subscription pages are protected

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,3 +1,11 @@
-export default function SubscriptionPage () {
-    return ( <h1>Subscription Page</h1> );
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function SubscriptionPage() {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return <h1>Subscription Page</h1>;
 }
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -17,6 +17,7 @@ export default async function TransactionsPage() {
       userId,
     },
   });
+
   return (
     <div>
       {/*NavBar*/}
