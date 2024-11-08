a9fffa8beff55ebd8b5a304308e5098ca780de9c
Wellington Lopes Nunes <wellington.nunes10@hotmail.com>
2024-11-08 15:18:17 -0300

    fix: upsert transaction function

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -27,11 +27,11 @@ export async function UpsertTransaction(params: UpsertTransactionParams) {
     throw new Error("Unauthorized");
   }
   await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
     where: {
       id: params.id ?? "",
     },
-    update: { ...params, userId },
-    create: { ...params, userId },
   });
   revalidatePath("/transactions");
 }
