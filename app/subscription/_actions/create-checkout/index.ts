ea283cec6044a1e851006f2aec2f02ff355f7054
Wellington Lopes Nunes <wellington.nunes10@hotmail.com>
2024-11-11 21:00:07 -0300

    feat: add stripe webhook that handles subscription

--- app/subscription/_actions/create-checkout/index.ts
+++ app/subscription/_actions/create-checkout/index.ts
@@ -3,25 +3,22 @@
 import { auth } from "@clerk/nextjs/server";
 import Stripe from "stripe";
 
-export default async function createStripeCheckout() {
+export const createStripeCheckout = async () => {
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
   if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Missing Stripe secret key");
+    throw new Error("Stripe secret key not found");
   }
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: "http://localhost:3000/",
-    cancel_url: "http://localhost:3000/",
+    success_url: "http://localhost:3000",
+    cancel_url: "http://localhost:3000",
     subscription_data: {
       metadata: {
         clerk_user_id: userId,
@@ -34,8 +31,5 @@ export default async function createStripeCheckout() {
       },
     ],
   });
-
-  return {
-    sessionId: session.id,
-  };
-}
+  return { sessionId: session.id };
+};
--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -1,25 +1,39 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import createStripeCheckout from "../_actions/create-checkout";
+import { createStripeCheckout } from "@/app/subscription/_actions/create-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function AcquirePlanButton() {
+const AcquirePlanButton = () => {
+  const { user } = useUser();
   const handleAcquirePlanClick = async () => {
-    // Implement the acquire plan logic here
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Missing Stripe publishable key");
+      throw new Error("Stripe publishable key not found");
     }
     const stripe = await loadStripe(
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     );
     if (!stripe) {
-      throw new Error("Stripe is not loaded");
+      throw new Error("Stripe not found");
     }
     await stripe.redirectToCheckout({ sessionId });
   };
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium";
 
+  if (hasPremiumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL}?prefilled_email=${JSON.stringify(user.emailAddresses[0].emailAddress)}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       className="w-full rounded-full font-bold"
@@ -28,4 +42,6 @@ export default function AcquirePlanButton() {
       Adquirir plano
     </Button>
   );
-}
+};
+
+export default AcquirePlanButton;
