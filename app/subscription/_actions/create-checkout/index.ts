4ab2f47c7154eb590e4677af8c803123918e2ec1
Wellington Lopes Nunes <wellington.nunes10@hotmail.com>
2024-11-12 21:59:05 -0300

    fix: ensure user is properly redirect after stripe checkout

--- app/subscription/_actions/create-checkout/index.ts
+++ app/subscription/_actions/create-checkout/index.ts
@@ -17,8 +17,8 @@ export const createStripeCheckout = async () => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: "http://localhost:3000",
-    cancel_url: "http://localhost:3000",
+    success_url: process.env.NEXT_PUBLIC_URL,
+    cancel_url: process.env.NEXT_PUBLIC_URL,
     subscription_data: {
       metadata: {
         clerk_user_id: userId,
